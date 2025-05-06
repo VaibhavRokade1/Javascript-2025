6b69863848f8fbd5d7c162492d836841a7e5e253
VaibhavRokade1 <rokadevaibhav04@.com>
2025-05-06 07:21:31 -0400

    Datatypes

---
+++ 01first.js
@@ -0,0 +1,18 @@
+console.log("Hey , I am Vaibhav Rokade");
+
+const accountID = 763765;
+let userName = "Vaibhav Rokade";
+var accountPassword = "vgcd@3gbkdc";
+var accountCity = "Jaypure";
+let demo;
+// reintialisation
+
+// console.log(accountID);
+
+console.table([accountID, accountCity, userName, demo]);
+
+/*
+prefer not to use var 
+
+because of issue in block and funtional scope.
+*/
---
+++ 02Datatype.js
@@ -0,0 +1,25 @@
+// Datatypes
+// "use strict"; treat all code as newer version of javascript
+
+// alert(3 + 3); we are using node.js , not browser
+
+// console.log(3
+//     +
+//     3); code readability that should be high
+
+// datatypes
+
+let name = "vaibhav rokade";
+let number = 87345786;
+let isLoggedin = true;
+
+// String => ""
+// Number => 657890 Range(2 to the power of 53 )
+// BigInt => mostly used in stock market
+// boolean => true / false
+//undefind = > not defind
+// null => it is an standalonge value
+// symbol => unique
+
+console.log(typeof undefined); //undefind
+console.log(typeof null); //object
