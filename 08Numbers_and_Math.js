2ed3ee61ea98350096ca89973ccbb64bf849976a
VaibhavRokade1 <rokadevaibhav04@.com>
2025-05-07 06:30:03 -0400

    Number s in Javascript

--- 08Numbers_and_Math.js
+++ 08Numbers_and_Math.js
@@ -3,6 +3,11 @@ let Numbers = 478;
 
 console.log(Numbers.toString());
 
-let anotherNumber = new Number(8973);
+let anotherNumber = new Number(8973.9834567);
 
-console.log(anotherNumber);
+console.log(anotherNumber.toFixed(2));
+console.log(anotherNumber.toPrecision(5));
+console.log(anotherNumber.toString().length);
+
+let Hunderads = 100000000000;
+console.log(Hunderads.toLocaleString("en-IN"));
