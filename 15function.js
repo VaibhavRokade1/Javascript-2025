ea80ca918b698d130f753e4659f47cfde0085409
VaibhavRokade1 <rokadevaibhav04@.com>
2025-05-09 08:01:32 -0400

    function and scope

--- 15function.js
+++ 15function.js
@@ -5,11 +5,34 @@
 // console.log(calculateCarPrice(200, 300, 400, 500));
 
 const userObj = {
-  nmae: "Vaibhav",
+  name: "Vaibhav",
   age: 22,
   email: " rokadevaibhav04gmail.com",
 };
 
-function Users() {
-  console.log(``);
+// function Users() {
+//   console.log(
+//     `Hey ${userObj.name} Your Age is : ${userObj.age} and email : ${userObj.email}`
+//   );
+// }
+
+// Users();
+
+const Users = [
+  {
+    name: "Vaibhav",
+    productName: "Buds",
+    productPrice: 203,
+  },
+  {
+    name: "Aditya",
+    productName: "penbox",
+    productPrice: 53,
+  },
+];
+
+function productTotal() {
+  return Users[0].productPrice + Users[0].productPrice;
 }
+
+console.log(productTotal());
---
+++ 16.scopes.js
@@ -0,0 +1,12 @@
+// Scope ( Local Scope  , Golable Scope)
+
+if (true) {
+  let a = 10;
+  const b = 20;
+  var c = 30;
+}
+
+// Printing State
+// console.log(a);
+// console.log(b);
+// console.log(c); var is not working at block level scope
