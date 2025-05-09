c9e54df87804f6e53b18f6c0b6a62a91342f1528
VaibhavRokade1 <rokadevaibhav04@.com>
2025-05-09 12:44:08 -0400

    ..

--- 16.scopes.js
+++ 16.scopes.js
@@ -10,3 +10,36 @@
 // console.log(a);
 // console.log(b);
 // console.log(c); var is not working at block level scope
+
+// if (true) {
+//   const userName = "Vaibhav";
+//   if (userName === "Vaibhav") {
+//     const tech = "Javascript";
+//     console.log(userName);
+//   }
+//   //   console.log(tech);
+// }
+
+// function one() {
+//   console.log("Hello World");
+//   function two() {
+//     console.log("Hay , Vaibhav");
+//   }
+//   two();
+// }
+// one();
+
+// ++++++++++++++++++ Intreasting +++++++++++++++++++++++
+
+// function dicleration type
+
+function mynew() {
+  console.log("Hello World");
+}
+// mynew();
+
+const mytest = function () {
+  console.log("Hello World");
+};
+
+// mytest();
---
+++ 17.thisinarrowfunction.js
@@ -0,0 +1,22 @@
+// This in arrow function
+
+const User = {
+  userName: "Vaibhav Rokade",
+  email: "rokadevaibhav04@gmail.com",
+  mydata: function () {
+    console.log(`Hey ${this.userName}`);
+  },
+};
+
+// User.mydata();
+// User.userName = "Vikas Pathade";
+// User.mydata();
+
+// console.log(this);   ==== > node.js document referece to the {}
+
+// This keyword is not working in plain function
+// ex :
+// function Chai() {
+//   console.log(this);
+// }
+// Chai();
