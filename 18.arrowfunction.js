b330f3017fdfcc4dce65dab9dca446f0fefdf3ce
VaibhavRokade1 <rokadevaibhav04@.com>
2025-05-09 12:57:13 -0400

    practice

---
+++ 18.arrowfunction.js
@@ -0,0 +1,13 @@
+// Arrow function
+
+// const chai = () => {
+//   console.log(this);
+// };
+// chai();
+
+const addTwo = (num1, num2) => {
+  return num1 + num2;
+};
+// console.log(addTwo(5, 6));
+
+// implicite return
