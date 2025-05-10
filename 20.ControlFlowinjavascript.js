dd44257e54211c1973a5b498b0169a9854a57a28
VaibhavRokade1 <rokadevaibhav04@.com>
2025-05-10 07:54:29 -0400

    ControlFlowinjavascript

--- 20.ControlFlowinjavascript.js
+++ 20.ControlFlowinjavascript.js
@@ -5,5 +5,18 @@
 const isLoggedin = true;
 
 if (isLoggedin) {
-  console.log("Executed");
+  //   console.log("Executed");
 }
+
+if (2 === "2") {
+  console.log("executed");
+}
+
+//  < , > , <= , >= , == , === ,!= ( Comparison Operator )
+
+const score = 200;
+if (score >= 100) {
+  const power = "fly";
+}
+
+console.log(`Your Power is  : ${power}`);
