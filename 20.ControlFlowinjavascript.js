563d40761d83b422af35632e9e2ef4ce4242bcd0
VaibhavRokade1 <rokadevaibhav04@.com>
2025-05-10 11:43:16 -0400

    control flow

--- 20.ControlFlowinjavascript.js
+++ 20.ControlFlowinjavascript.js
@@ -20,3 +20,34 @@ const score = 200;
 // }
 
 // console.log(`Your Power is  : ${power}`);
+
+const balance = 1000;
+
+// if (balance > 500) console.log("Hello world"), console.log("False"); this is very bad syntax for the indstry standerd is not recommended by to use in our project
+
+// if (balance < 200) {
+//   console.log("Balance is less than '200'");
+// } else if (balance > 200) {
+//   console.log("Balance is Greater than '200'");
+// } else {
+//   console.log("Enter a valid value in variable");
+// }
+
+const userLoggedIn = true;
+const isDebitcard = true;
+const isEmailLoggedIn = true;
+const isGoogleLoggedIn = false;
+
+// if (userLoggedIn && isDebitcard && isEmailLoggedIn) {
+//   console.log("Allow to the shopping.");
+// }
+
+// if (userLoggedIn && isGoogleLoggedIn && isEmailLoggedIn) {
+//   console.log("Allow to the shopping.");
+// } else {
+//   console.log("Ho Ghary Bhikary");
+// }
+
+// if (isDebitcard || isGoogleLoggedIn) {
+//   console.log(" Karu adjust yeundya ");
+// }
