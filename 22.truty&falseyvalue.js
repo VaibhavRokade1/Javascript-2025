24eed94ba1004ce639db86e504a80cc626863845
VaibhavRokade1 <rokadevaibhav04@.com>
2025-05-10 13:29:06 -0400

    Practice (control flow , ?? , turnary operator , truethy values , falsy values)

--- 22.truty&falseyvalue.js
+++ 22.truty&falseyvalue.js
@@ -3,7 +3,36 @@
 const massage = [];
 
 if (massage) {
-  console.log("Massage is according to  : truety values");
+  //   console.log("Massage is according to  : truety values");
 } else {
-  console.log("Massage is according to  : falsey values");
+  //   console.log("Massage is according to  : falsey values");
 }
+
+// falsey values
+
+// false , 0 , -0 , 0n , "" , [] , null ,undefind , null , NaN
+
+//Truthy value
+// true  1 , "0", 'false' , function(){} , {}
+
+const emptyObject = {};
+
+if (Object.keys(emptyObject).length === 0) {
+  //   console.log("Object is empty");
+}
+
+// nullish coalescing operator ?? : null , undefind
+
+const chai = 23 ?? 73;
+const chai1 = null ?? 90;
+const chai2 = undefined ?? 73;
+
+console.log(chai);
+console.log(chai1);
+console.log(chai2);
+
+// tarnary operator
+
+// condition ? true : false;
+
+// 1 == true ? console.log("True") : console.log("False");
