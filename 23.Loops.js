5630663057cde4b7ec68a5359f6e207d9f60413e
VaibhavRokade1 <rokadevaibhav04@.com>
2025-05-11 12:41:35 -0400

    practice

---
+++ 23.Loops.js
@@ -0,0 +1,167 @@
+// For Loop in javascript
+
+// for (let i = 0; i <= 10; i++) {
+//   if (i == 5) {
+//     console.log(`${i}  :is printing done`);
+//   }
+//   console.log(i);
+// }
+
+// Nested Loops
+
+// for (let i = 0; i <= 10; i++) {
+//   for (let j = 0; j <= 10; j++) {
+//     console.log(`Inner Loop Value is : ${j} , Outer Loop Value is :${i}`);
+//   }
+//   console.log("\n");
+// }
+
+// Output
+// PS V:\Javascript-2025> node .\23.Loops.js
+// Inner Loop Value is : 0 , Outer Loop Value is :0
+// Inner Loop Value is : 1 , Outer Loop Value is :0
+// Inner Loop Value is : 2 , Outer Loop Value is :0
+// Inner Loop Value is : 3 , Outer Loop Value is :0
+// Inner Loop Value is : 4 , Outer Loop Value is :0
+// Inner Loop Value is : 5 , Outer Loop Value is :0
+// Inner Loop Value is : 6 , Outer Loop Value is :0
+// Inner Loop Value is : 7 , Outer Loop Value is :0
+// Inner Loop Value is : 8 , Outer Loop Value is :0
+// Inner Loop Value is : 9 , Outer Loop Value is :0
+// Inner Loop Value is : 10 , Outer Loop Value is :0
+
+// Inner Loop Value is : 0 , Outer Loop Value is :1
+// Inner Loop Value is : 1 , Outer Loop Value is :1
+// Inner Loop Value is : 2 , Outer Loop Value is :1
+// Inner Loop Value is : 3 , Outer Loop Value is :1
+// Inner Loop Value is : 4 , Outer Loop Value is :1
+// Inner Loop Value is : 5 , Outer Loop Value is :1
+// Inner Loop Value is : 6 , Outer Loop Value is :1
+// Inner Loop Value is : 7 , Outer Loop Value is :1
+// Inner Loop Value is : 8 , Outer Loop Value is :1
+// Inner Loop Value is : 9 , Outer Loop Value is :1
+// Inner Loop Value is : 10 , Outer Loop Value is :1
+
+// Inner Loop Value is : 0 , Outer Loop Value is :2
+// Inner Loop Value is : 1 , Outer Loop Value is :2
+// Inner Loop Value is : 2 , Outer Loop Value is :2
+// Inner Loop Value is : 3 , Outer Loop Value is :2
+// Inner Loop Value is : 4 , Outer Loop Value is :2
+// Inner Loop Value is : 5 , Outer Loop Value is :2
+// Inner Loop Value is : 6 , Outer Loop Value is :2
+// Inner Loop Value is : 7 , Outer Loop Value is :2
+// Inner Loop Value is : 8 , Outer Loop Value is :2
+// Inner Loop Value is : 9 , Outer Loop Value is :2
+// Inner Loop Value is : 10 , Outer Loop Value is :2
+
+// Inner Loop Value is : 0 , Outer Loop Value is :3
+// Inner Loop Value is : 1 , Outer Loop Value is :3
+// Inner Loop Value is : 2 , Outer Loop Value is :3
+// Inner Loop Value is : 3 , Outer Loop Value is :3
+// Inner Loop Value is : 4 , Outer Loop Value is :3
+// Inner Loop Value is : 5 , Outer Loop Value is :3
+// Inner Loop Value is : 6 , Outer Loop Value is :3
+// Inner Loop Value is : 7 , Outer Loop Value is :3
+// Inner Loop Value is : 8 , Outer Loop Value is :3
+// Inner Loop Value is : 9 , Outer Loop Value is :3
+// Inner Loop Value is : 10 , Outer Loop Value is :3
+
+// Inner Loop Value is : 0 , Outer Loop Value is :4
+// Inner Loop Value is : 1 , Outer Loop Value is :4
+// Inner Loop Value is : 2 , Outer Loop Value is :4
+// Inner Loop Value is : 3 , Outer Loop Value is :4
+// Inner Loop Value is : 4 , Outer Loop Value is :4
+// Inner Loop Value is : 5 , Outer Loop Value is :4
+// Inner Loop Value is : 6 , Outer Loop Value is :4
+// Inner Loop Value is : 7 , Outer Loop Value is :4
+// Inner Loop Value is : 8 , Outer Loop Value is :4
+// Inner Loop Value is : 9 , Outer Loop Value is :4
+// Inner Loop Value is : 10 , Outer Loop Value is :4
+
+// Inner Loop Value is : 0 , Outer Loop Value is :5
+// Inner Loop Value is : 1 , Outer Loop Value is :5
+// Inner Loop Value is : 2 , Outer Loop Value is :5
+// Inner Loop Value is : 3 , Outer Loop Value is :5
+// Inner Loop Value is : 4 , Outer Loop Value is :5
+// Inner Loop Value is : 5 , Outer Loop Value is :5
+// Inner Loop Value is : 6 , Outer Loop Value is :5
+// Inner Loop Value is : 7 , Outer Loop Value is :5
+// Inner Loop Value is : 8 , Outer Loop Value is :5
+// Inner Loop Value is : 9 , Outer Loop Value is :5
+// Inner Loop Value is : 10 , Outer Loop Value is :5
+
+// Inner Loop Value is : 0 , Outer Loop Value is :6
+// Inner Loop Value is : 1 , Outer Loop Value is :6
+// Inner Loop Value is : 2 , Outer Loop Value is :6
+// Inner Loop Value is : 3 , Outer Loop Value is :6
+// Inner Loop Value is : 4 , Outer Loop Value is :6
+// Inner Loop Value is : 5 , Outer Loop Value is :6
+// Inner Loop Value is : 6 , Outer Loop Value is :6
+// Inner Loop Value is : 7 , Outer Loop Value is :6
+// Inner Loop Value is : 8 , Outer Loop Value is :6
+// Inner Loop Value is : 9 , Outer Loop Value is :6
+// Inner Loop Value is : 10 , Outer Loop Value is :6
+
+// Inner Loop Value is : 0 , Outer Loop Value is :7
+// Inner Loop Value is : 1 , Outer Loop Value is :7
+// Inner Loop Value is : 2 , Outer Loop Value is :7
+// Inner Loop Value is : 3 , Outer Loop Value is :7
+// Inner Loop Value is : 4 , Outer Loop Value is :7
+// Inner Loop Value is : 5 , Outer Loop Value is :7
+// Inner Loop Value is : 6 , Outer Loop Value is :7
+// Inner Loop Value is : 7 , Outer Loop Value is :7
+// Inner Loop Value is : 8 , Outer Loop Value is :7
+// Inner Loop Value is : 9 , Outer Loop Value is :7
+// Inner Loop Value is : 10 , Outer Loop Value is :7
+
+// Inner Loop Value is : 0 , Outer Loop Value is :8
+// Inner Loop Value is : 1 , Outer Loop Value is :8
+// Inner Loop Value is : 2 , Outer Loop Value is :8
+// Inner Loop Value is : 3 , Outer Loop Value is :8
+// Inner Loop Value is : 4 , Outer Loop Value is :8
+// Inner Loop Value is : 5 , Outer Loop Value is :8
+// Inner Loop Value is : 6 , Outer Loop Value is :8
+// Inner Loop Value is : 7 , Outer Loop Value is :8
+// Inner Loop Value is : 8 , Outer Loop Value is :8
+// Inner Loop Value is : 9 , Outer Loop Value is :8
+// Inner Loop Value is : 10 , Outer Loop Value is :8
+
+// Inner Loop Value is : 0 , Outer Loop Value is :9
+// Inner Loop Value is : 1 , Outer Loop Value is :9
+// Inner Loop Value is : 2 , Outer Loop Value is :9
+// Inner Loop Value is : 3 , Outer Loop Value is :9
+// Inner Loop Value is : 4 , Outer Loop Value is :9
+// Inner Loop Value is : 5 , Outer Loop Value is :9
+// Inner Loop Value is : 6 , Outer Loop Value is :9
+// Inner Loop Value is : 7 , Outer Loop Value is :9
+// Inner Loop Value is : 8 , Outer Loop Value is :9
+// Inner Loop Value is : 9 , Outer Loop Value is :9
+// Inner Loop Value is : 10 , Outer Loop Value is :9
+
+// Inner Loop Value is : 0 , Outer Loop Value is :10
+// Inner Loop Value is : 1 , Outer Loop Value is :10
+// Inner Loop Value is : 2 , Outer Loop Value is :10
+// Inner Loop Value is : 3 , Outer Loop Value is :10
+// Inner Loop Value is : 4 , Outer Loop Value is :10
+// Inner Loop Value is : 5 , Outer Loop Value is :10
+// Inner Loop Value is : 6 , Outer Loop Value is :10
+// Inner Loop Value is : 7 , Outer Loop Value is :10
+// Inner Loop Value is : 8 , Outer Loop Value is :10
+// Inner Loop Value is : 9 , Outer Loop Value is :10
+// Inner Loop Value is : 10 , Outer Loop Value is :10
+
+// for (let i = 0; i <= 10; i++) {
+//   for (let j = 0; j <= i; j++) {
+//     console.log(i + " * " + j + " = " + i * j);
+//   }
+// }
+
+// const myArray = ["flash", "Superman", "Spiderman"];
+// console.log(myArray.length);
+
+// for (let index = 0; index < myArray.length; index++) {
+//   const element = myArray[index];
+//   console.log(element);
+// }
+
+for (let i = 1; i <= 10; i++) {}
