5a123a4f518757ff894b1517812216a0d9b58cf6
VaibhavRokade1 <rokadevaibhav04@.com>
2025-05-12 07:50:12 -0400

    basic javascript is over

---
+++ 28.foreachloop.js
@@ -0,0 +1,43 @@
+// Foreach loop
+
+// const myArr = ["Javascript", "Java", "Ruby", "python"];
+
+// const values = myArr.forEach((item) => {
+//   return item;
+// });
+
+// console.log(values);  // return undefind
+
+// const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let newNum = [];
+
+// const newNum = myNum.filter((item) => item >= 5);
+
+// Same using foreach loop
+
+// myNum.forEach((num) => {
+//   if (num >= 5) {
+//     newNum.push(num);
+//   }
+// });
+
+// console.log(newNum);
+
+// map with array in javascript
+// const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const newNumber = myNum.map((num) => {
+//   return num + 10;
+// });
+
+// console.log(newNumber);
+
+// const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const chaining = myNum
+//   .map((num) => num + 10)
+//   .map((num) => num + 1)
+//   .filter((num) => num >= 40);
+
+// console.log(chaining);
---
+++ 29.redeuce.js
@@ -0,0 +1,37 @@
+// reduce method in javascript
+const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let total = myNum.reduce((accumelater, currectValue) => {
+//   console.log(`acc : ${accumelater} current : ${currectValue}`);
+//   return accumelater + currectValue;
+// }, 0);
+
+// let total = myNum.reduce((acc, curr) => {
+//   return acc + curr;
+// });
+
+// console.log(total);
+
+// const shopingCard = [
+//   {
+//     courseName: "Js course",
+//     coursePrice: 2000,
+//   },
+//   {
+//     courseName: ".Net course",
+//     coursePrice: 6000,
+//   },
+//   {
+//     courseName: "Java course",
+//     coursePrice: 9000,
+//   },
+// ];
+
+// const totalPriceToPay = shopingCard.reduce(
+//   (acc, sp) => acc + sp.coursePrice,
+//   0
+// );
+
+// console.log(totalPriceToPay);
+
+// javascript Basic is over
